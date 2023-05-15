b7980aaa40459a9c8bf4552fb0bc54951f18b43e
raja0096 <132909456+raja0096@users.noreply.github.com>
2023-05-15 09:38:35 -0400

    Create index.js
    JavaScript file created and JS content added.

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//TASK 1
+let a =3;
+let b =5;
+let c;
+
+// alert( "description of alerted_value: " + alerted_value );
+let output=`
+let a =3;\n
+let b =5;\n
+let c;
+------------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+
+`;
+alert(output);
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+
+//TASK 2
