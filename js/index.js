a330b42e7ad36e5bf7b301fa19d34c44a0bb63f5
raja0096 <132909456+raja0096@users.noreply.github.com>
2023-05-17 15:37:45 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,18 +4,32 @@ let b =5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output=`
-let a =3;\n
-let b =5;\n
+let print=`
+let a =3;
+let b =5;
 let c;
 ------------
 a + b = ${a + b}
 a - b = ${a - b}
 a * b = ${a * b}
 a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c} 
+!a || !c = ${!a || !c}
 
 `;
-alert(output);
+
+alert(print);
+
 /*
 a + b
 a - b
@@ -35,5 +49,11 @@ a < b
 !a || !c 
 */
 
-
 //TASK 2
+let first_name= 'Niraimoli';
+let last_name= 'Rajan';
+let email= 'raja0096@algonquinlive.com';
+let output=`My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+
+alert(output);
